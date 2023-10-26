b72c104e79c1bd4257666428f3d2a415fb0267bc
difer10 <dfclaros@unicauca.edu.co>
2023-10-26 01:36:59 -0500

    cbv create

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
-from .views import PageListView
+from .views import PageDetailView, PageListView, PageCreate
 
-urlpatterns = [
+pages_patterns = ([
     path('', PageListView.as_view(), name='pages'),
-    path('<int:page_id>/<slug:page_slug>/', views.page, name='page'),
-]
+    path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
+    path('create/', PageCreate.as_view(), name='create'),
+],'pages')
