f2312c6bfc89697523c17efc48d37120bb838de7
difer10 <dfclaros@unicauca.edu.co>
2023-10-26 03:02:03 -0500

    cbv update

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PageDetailView, PageListView, PageCreate
+from .views import PageDetailView, PageListView, PageCreate,PageUpdate
 
 pages_patterns = ([
     path('', PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
     path('create/', PageCreate.as_view(), name='create'),
+    path('update/<int:pk>', PageUpdate.as_view(), name='update'),
 ],'pages')
