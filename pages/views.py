944f8061f9c534c7a2e3bd7a0042bf69e578aa05
difer10 <dfclaros@unicauca.edu.co>
2023-10-30 11:43:10 -0500

    seguridad en autentificacion para create, update,delete

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,20 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.urls import reverse, reverse_lazy
+from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForm
+
+#creando un mixin para autentificacion
+class StaffRequiredMixin(object):
+   """Este Mixin requerirá que el usuario sea miembro del staff"""
+
+   @method_decorator(staff_member_required)
+   def dispatch(self, request, *args, **kwargs):
+      return super(StaffRequiredMixin, self). dispatch(request, *args, **kwargs)
 
 # Create your views here.
 class PageDetailView (DetailView):
@@ -12,11 +23,13 @@ class PageDetailView (DetailView):
 class PageListView (ListView):
    model=Page
 
+@method_decorator(staff_member_required,name='dispach')
 class PageCreate(CreateView):
     model = Page
     form_class=PageForm
     success_url=  reverse_lazy('pages:pages')
 
+@method_decorator(staff_member_required,name='dispach')
 class PageUpdate(UpdateView):
     model = Page
     form_class=PageForm
@@ -24,7 +37,8 @@ class PageUpdate(UpdateView):
         
     def get_success_url(self) -> str:
        return reverse_lazy('pages:update',args=[self.object.id])+ '?ok'
-    
+
+@method_decorator(staff_member_required,name='dispach')    
 class PageDelete(DeleteView):
    model=Page
    success_url= reverse_lazy('pages:pages')
