c622ab5f8102f102064f9976f1a58457a7b68c94
difer10 <dfclaros@unicauca.edu.co>
2023-10-30 15:57:08 -0500

    restauracion del password

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -131,3 +131,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Auth redirects
 LOGIN_REDIRECT_URL ='pages:pages'
 LOGOUT_REDIRECT_URL ='home'
+
+# Emails
+if DEBUG:
+    EMAIL_BACKEND= "django.core.mail.backends.filebased.EmailBackend"
+    EMAIL_FILE_PATH= BASE_DIR/ 'sent_emails'
+else:
+    #aqui hay que configurar un email real para produccion (credenciales del correo admin)
+    pass
+
